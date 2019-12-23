92c6a8f6e0bc06f1678b6b6adca6fc1677cf0934
jlx-666 <970576061@qq.com>
2019-12-23 15:41:38 +0800

    完善个人收藏功能

---
+++ pages/mine/myCollections/myCollections.js
@@ -0,0 +1,51 @@
+// pages/mine/myCollections/myCollections.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    paperbase:null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this
+    wx.request({
+      url: 'http://127.0.0.1:8080/getMyCollections',
+      data:{
+        openId: getApp().globalData.openid
+      },
+      method:'POST',
+      header:{
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success:function(res){
+        var paperbase = res.data
+        console.log(res.data)
+        that.setData({
+          paperbase:paperbase
+        })
+      }
+    })
+  },
+  goExercise: function (e) {
+    var id = e.currentTarget.dataset.id;
+    wx.request({
+      url: 'http://127.0.0.1:8080/getById',
+      data: {
+        id: id
+      },
+      success: function (res) {
+        wx.setStorageSync("paper", res.data)
+        console.log(res.data)
+        wx.navigateTo({
+          url: '/pages/doExercise/doExercise',
+        })
+      }
+    })
+  },
+ 
+})
--- pages/userInfo/userInfo.js
+++ pages/userInfo/userInfo.js
@@ -17,54 +17,14 @@ Page({
       userInfo: app.globalData.userInfo,
       hasUserInfo: true
     })
+    console.log(this.data.userInfo)
+    console.log(app.globalData.openid)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  goCollection:function(){
+    wx.navigateTo({
+      url: '/pages/mine/myCollections/myCollections',
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
